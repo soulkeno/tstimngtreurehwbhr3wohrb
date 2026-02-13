9c1a0394c2756647faedcf2254b033f54b05c97f
Keno <129610358+soulkeno@users.noreply.github.com>
2026-02-13 23:48:17 +0100

    Reorder imports and render App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
-
 const params = new URLSearchParams(window.location.search);
 const redirectPath = params.get("p");
 
 if (redirectPath) {
   window.history.replaceState(null, "", redirectPath);
 }
+
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(<App />);
