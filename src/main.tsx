8cec34ffbe85f7c527e118bf89643489720b6c7c
Keno <129610358+soulkeno@users.noreply.github.com>
2026-02-13 23:47:46 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,10 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get("p");
+
+if (redirectPath) {
+  window.history.replaceState(null, "", redirectPath);
+}
